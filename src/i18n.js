8bed79937878090bc0b0d555778e0c7a8410ef7b
Lakshmi Nelapogula <lakshminelapogula@gmail.com>
2026-09-11 00:54:45 +0530

    Fix Vite import resolution error for i18n adapter

--- src/i18n.js
+++ src/i18n.js
@@ -1,23 +1,52 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 import { translations } from './data/translations';
 
-// i18next expects resources to be nested under a default namespace (usually 'translation')
+// Lightweight i18n adapter backed by KisanQueue translations system
 const resources = Object.keys(translations).reduce((acc, lang) => {
   acc[lang] = { translation: translations[lang] };
   return acc;
 }, {});
 
-i18n
-  .use(initReactI18next)
-  .init({
+let currentLanguage = 'en';
+const listeners = new Set();
+
+const i18n = {
+  language: currentLanguage,
+  languages: Object.keys(translations),
+  options: {
     resources,
-    lng: 'en',
     fallbackLng: 'en',
-    supportedLngs: ['en', 'hi', 'te', 'ta', 'kn', 'ml', 'mr', 'bn'],
-    interpolation: {
-      escapeValue: false
+  },
+  changeLanguage: (lang) => {
+    if (translations[lang]) {
+      currentLanguage = lang;
+      i18n.language = lang;
+      listeners.forEach((fn) => fn(lang));
+      return Promise.resolve(lang);
     }
-  });
+    return Promise.reject(new Error(`Language ${lang} not supported`));
+  },
+  t: (key, paramsOrFallback = {}) => {
+    let params = typeof paramsOrFallback === 'object' ? paramsOrFallback : {};
+    let text = translations[currentLanguage]?.[key] || translations.en?.[key] || (typeof paramsOrFallback === 'string' ? paramsOrFallback : key);
+    if (params && typeof text === 'string') {
+      Object.keys(params).forEach((paramKey) => {
+        const doubleRegex = new RegExp(`{{\\s*${paramKey}\\s*}}`, 'g');
+        text = text.replace(doubleRegex, String(params[paramKey]));
+      });
+    }
+    return text;
+  },
+  on: (event, callback) => {
+    if (event === 'languageChanged') {
+      listeners.add(callback);
+    }
+  },
+  off: (event, callback) => {
+    if (event === 'languageChanged') {
+      listeners.delete(callback);
+    }
+  }
+};
 
 export default i18n;
+
